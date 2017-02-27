7c547eb40e9b3422baafed4fa0b4e85fe456ff27
Joanna Kao <joannaskao@gmail.com>
2017-02-27 06:07:52 -0500

    try to use react/reactdom

--- client/index.js
+++ client/index.js
@@ -1,7 +1,21 @@
-import annotations from './components/annotations';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// import annotations from './components/annotations';
+//
+// annotations.init('[data-annotation-text]', {
+//   minWidth: 200,
+//   maxWidth: 400,
+//   gutter: 40,
+// });
 
-annotations.init('[data-annotation-text]', {
-  minWidth: 200,
-  maxWidth: 400,
-  gutter: 40
-});
+class Annotation extends Component {
+  render() {
+    const annotationText = 'Hello test';
+    const authorName = 'Test';
+    const authorLink = 'http://google.com';
+
+    return <aside id="annotation" aria-hidden="false" aria-live="polite" class="speech__annotation" style="width: 100%; top: 271px; left: 669px; visibility: visible;"><p>{ annotationText }</p><a href="{ authorLink }" rel="author" class="speech__annotation-byline">{ authorName }</a></aside>;
+  }
+}
+
+ReactDOM.render(<Annotation />, document.getElementById('speech-container'));
