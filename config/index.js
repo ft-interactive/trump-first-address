12cd058cbd506ebe5cd47ec23c7dbecfe20ffdad
Joanna Kao <joannaskao@gmail.com>
2017-02-26 23:24:45 -0500

    try to use preact

--- config/index.js
+++ config/index.js
@@ -1,7 +1,10 @@
+/** @jsx preact.h */
 import article from './article';
 import getFlags from './flags';
 import getOnwardJourney from './onward-journey';
 import axios from 'axios';
+import { h, render, Component } from 'preact';
+
 
 function annotateSpeech(annotations) {
   let manipulatedHtml = speechBody.html;
@@ -11,6 +14,16 @@ function annotateSpeech(annotations) {
 
   return manipulatedHtml;
 }
+
+class Annotation extends Component {
+  render() {
+    const annotation_text = "Hello test";
+    return <aside id="annotation" aria-hidden="false" aria-live="polite" class="speech__annotation" style="width: 100%; top: 271px; left: 669px; visibility: visible;"><p>{ annotation_text }</p><a href="{ author_link }" rel="author" class="speech__annotation-byline">{ author_name }</a></aside>;
+  }
+}
+
+render(<Annotation />, document.getElementById('speech-container'));
+
 
 export default async function() {
   const d = await article();
